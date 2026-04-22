b1302b69743a933edbe56c74ec730c95f4673ce4
jaxjoelliott <jaxjoelliott33@gmail.com>
2026-04-22 15:24:52 -0400

    Added lint no-used-vars rule to recognize _prefix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,10 @@ export default [
   ...tseslint.configs.recommended,
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { caughtErrorsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/explicit-function-return-type': 'warn',
     },
   },

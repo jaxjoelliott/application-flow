1a95e7d9bfdc16f208d69b8c4da9363ec6697895
Jackson Elliott <101903586+jaxjoelliott@users.noreply.github.com>
2026-04-30 14:52:03 -0400

    chore: Made s3 bucket backend for Terraform (#11)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 
 
 terraform {
+  backend "s3" {
+    bucket = "applicationflow-dev-jackson"
+    key    = "applicationflow/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"

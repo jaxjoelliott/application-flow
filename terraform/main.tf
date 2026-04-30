724385a37aec888a4d0cb242a357b217a7e9e39f
jaxjoelliott <jaxjoelliott33@gmail.com>
2026-04-30 12:03:01 -0400

    Added rest of Lambda functions as code

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region = var.region_name
 }
 
+// IAM Role for Lambda function with basic permissions and Put_Item_Policy
 resource "aws_iam_role" "basic_lambda_role" {
   name = "application-flow-lambda-role"
 
@@ -29,7 +30,7 @@ resource "aws_iam_role" "basic_lambda_role" {
     ]
   })
 }
-
+// Policy to allow Lambda function to put items into job-applications table
 resource "aws_iam_policy" "Put_Item_Policy" {
   name        = "PutItemPolicy"
   path        = "/"
@@ -45,23 +46,131 @@ resource "aws_iam_policy" "Put_Item_Policy" {
           "dynamodb:PutItem"
         ]
         Effect   = "Allow"
-        Resource = aws_dynamodb_table.basic_dynamodb_table.arn
+        Resource = aws_dynamodb_table.job_applications_table.arn
       },
     ]
   })
 }
 
+
+// Policy to allow Lambda function to list items from job-applications table
+resource "aws_iam_policy" "List_Items_Policy" {
+  name        = "ListItemsPolicy"
+  path        = "/"
+  description = "DynamoDB ListItems policy for Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:Scan"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.job_applications_table.arn
+      },
+    ]
+  })
+}
+
+// Policy to allow Lambda function to get item from job-applications table
+resource "aws_iam_policy" "Get_Item_Policy" {
+  name        = "GetItemPolicy"
+  path        = "/"
+  description = "DynamoDB GetItem policy for Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.job_applications_table.arn
+      },
+    ]
+  })
+}
+
+// Policy to allow Lambda function to update item in job-applications table
+resource "aws_iam_policy" "Update_Item_Policy" {
+  name        = "UpdateItemPolicy"
+  path        = "/"
+  description = "DynamoDB UpdateItem policy for Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:UpdateItem"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.job_applications_table.arn
+      },
+    ]
+  })
+}
+
+// Policy to allow Lambda function to delete item from job-applications table
+resource "aws_iam_policy" "Delete_Item_Policy" {
+  name        = "DeleteItemPolicy"
+  path        = "/"
+  description = "DynamoDB DeleteItem policy for Lambda function"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:DeleteItem"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.job_applications_table.arn
+      },
+    ]
+  })
+}
+
+// Attach AWSLambdaBasicExecutionRole to basic_lambda_role
 resource "aws_iam_role_policy_attachment" "basic_lambda_policy_attachment" {
   role     = aws_iam_role.basic_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+// Attach Put_Item_Policy to basic_lambda_role
 resource "aws_iam_role_policy_attachment" "DynamoDBPutItemPolicyAttachment" {
   role     = aws_iam_role.basic_lambda_role.name
   policy_arn = aws_iam_policy.Put_Item_Policy.arn
 }
 
-resource "aws_dynamodb_table" "basic_dynamodb_table" {
+// Attach List_Items_Policy to basic_lambda_role
+resource "aws_iam_role_policy_attachment" "DynamoDBListItemsPolicyAttachment" {
+  role     = aws_iam_role.basic_lambda_role.name
+  policy_arn = aws_iam_policy.List_Items_Policy.arn
+}
+
+// Attach Get_Item_Policy to basic_lambda_role
+resource "aws_iam_role_policy_attachment" "DynamoDBGetItemPolicyAttachment" {
+  role     = aws_iam_role.basic_lambda_role.name
+  policy_arn = aws_iam_policy.Get_Item_Policy.arn
+}
+
+// Attach Update_Item_Policy to basic_lambda_role
+resource "aws_iam_role_policy_attachment" "DynamoDBUpdateItemPolicyAttachment" {
+  role     = aws_iam_role.basic_lambda_role.name
+  policy_arn = aws_iam_policy.Update_Item_Policy.arn
+}
+
+//. Attach Delete_Item_Policy to basic_lambda_role
+resource "aws_iam_role_policy_attachment" "DynamoDBDeleteItemPolicyAttachment" {
+  role     = aws_iam_role.basic_lambda_role.name
+  policy_arn = aws_iam_policy.Delete_Item_Policy.arn
+}
+
+// DynamoDB table resource for job applications
+resource "aws_dynamodb_table" "job_applications_table" {
   name = "job-applications"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
@@ -71,7 +180,8 @@ resource "aws_dynamodb_table" "basic_dynamodb_table" {
   }
 }
 
-resource "aws_lambda_function" "basic_lambda_function" {
+// Lambda function resource createApplication
+resource "aws_lambda_function" "create_application" {
   function_name = "createApplication"
   role          = aws_iam_role.basic_lambda_role.arn
   handler       = "dist/handlers/createApplication.handler"
@@ -81,13 +191,70 @@ resource "aws_lambda_function" "basic_lambda_function" {
 
   environment {
     variables = {
-      TABLE_NAME = aws_dynamodb_table.basic_dynamodb_table.name
+      TABLE_NAME = aws_dynamodb_table.job_applications_table.name
+    }
+  }
+}
+// Lambda function resource listApplications
+resource "aws_lambda_function" "list_applications" {
+  function_name = "listApplications"
+  role          = aws_iam_role.basic_lambda_role.arn
+  handler       = "dist/handlers/listApplications.handler"
+  runtime       = "nodejs22.x"
+
+  filename      = "function.zip"
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.job_applications_table.name
     }
   }
 }
 
+// Lambda function resource getApplication
+resource "aws_lambda_function" "get_application" {
+  function_name = "getApplication"
+  role          = aws_iam_role.basic_lambda_role.arn
+  handler       = "dist/handlers/getApplication.handler"
+  runtime       = "nodejs22.x"
 
+  filename      = "function.zip"
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.job_applications_table.name
+    }
+  }
+}
 
+// Lambda function resource updateApplication
+resource "aws_lambda_function" "update_application" {
+  function_name = "updateApplication"
+  role          = aws_iam_role.basic_lambda_role.arn
+  handler       = "dist/handlers/updateApplication.handler"
+  runtime       = "nodejs22.x"
+
+  filename      = "function.zip"
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.job_applications_table.name
+    }
+  }
+}
+
+// Lambda function resource deleteApplication
+resource "aws_lambda_function" "delete_application" {
+  function_name = "deleteApplication"
+  role          = aws_iam_role.basic_lambda_role.arn
+  handler       = "dist/handlers/deleteApplication.handler"
+  runtime       = "nodejs22.x"
+
+  filename      = "function.zip"
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.job_applications_table.name
+    }
+  }
+}
 
 
 

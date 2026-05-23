52bc294a5ac1940f3604630492120618ece6005a
jaxjoelliott <jaxjoelliott33@gmail.com>
2026-05-23 18:14:02 -0400

    Refactored IAM role attachments for Lambdas

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 
-
+// Terraform configuration for AWS resources
 terraform {
   backend "s3" {
     bucket = "applicationflow-dev-jackson"
@@ -18,9 +18,9 @@ provider "aws" {
   region = var.region_name
 }
 
-// IAM Role for Lambda function with basic permissions and Put_Item_Policy
-resource "aws_iam_role" "basic_lambda_role" {
-  name = "application-flow-lambda-role"
+// IAM Role for createApplication with basic permissions and Put_Item_Policy
+resource "aws_iam_role" "create_application_lambda_role" {
+  name = "createApplication-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -35,11 +35,87 @@ resource "aws_iam_role" "basic_lambda_role" {
     ]
   })
 }
+
+// IAM Role for listApplications with basic permissions and List_Item_Policy
+resource "aws_iam_role" "list_applications_lambda_role" {
+  name = "listApplications-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+// IAM Role for getApplication with basic permissions and Get_Item_Policy
+
+resource "aws_iam_role" "get_application_lambda_role" {
+  name = "getApplication-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+// IAM Role for updateApplication with basic permissions and Update_Item_Policy
+
+resource "aws_iam_role" "update_application_lambda_role" {
+  name = "updateApplication-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+// IAM Role for deleteApplication with basic permissions and Delete_Item_Policy
+
+resource "aws_iam_role" "delete_application_lambda_role" {
+  name = "deleteApplication-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
 // Policy to allow Lambda function to put items into job-applications table
 resource "aws_iam_policy" "Put_Item_Policy" {
   name        = "PutItemPolicy"
   path        = "/"
-  description = "DynamoDB PutItem policy for Lambda function"
+  description = "DynamoDB PutItem policy for createApplication Lambda function"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -62,7 +138,7 @@ resource "aws_iam_policy" "Put_Item_Policy" {
 resource "aws_iam_policy" "List_Items_Policy" {
   name        = "ListItemsPolicy"
   path        = "/"
-  description = "DynamoDB ListItems policy for Lambda function"
+  description = "DynamoDB ListItems policy for listApplications Lambda function"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -82,7 +158,7 @@ resource "aws_iam_policy" "List_Items_Policy" {
 resource "aws_iam_policy" "Get_Item_Policy" {
   name        = "GetItemPolicy"
   path        = "/"
-  description = "DynamoDB GetItem policy for Lambda function"
+  description = "DynamoDB GetItem policy for getApplication Lambda function"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -102,7 +178,7 @@ resource "aws_iam_policy" "Get_Item_Policy" {
 resource "aws_iam_policy" "Update_Item_Policy" {
   name        = "UpdateItemPolicy"
   path        = "/"
-  description = "DynamoDB UpdateItem policy for Lambda function"
+  description = "DynamoDB UpdateItem policy for updateApplication Lambda function"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -122,7 +198,7 @@ resource "aws_iam_policy" "Update_Item_Policy" {
 resource "aws_iam_policy" "Delete_Item_Policy" {
   name        = "DeleteItemPolicy"
   path        = "/"
-  description = "DynamoDB DeleteItem policy for Lambda function"
+  description = "DynamoDB DeleteItem policy for deleteApplication Lambda function"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -138,39 +214,73 @@ resource "aws_iam_policy" "Delete_Item_Policy" {
   })
 }
 
-// Attach AWSLambdaBasicExecutionRole to basic_lambda_role
-resource "aws_iam_role_policy_attachment" "basic_lambda_policy_attachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+// createApplication role attachments
+
+// Attach basic Lambda execution policy to create_application_lambda_role
+resource "aws_iam_role_policy_attachment" "createApplication_basic_lambda_policy_attachment" {
+  role     = aws_iam_role.create_application_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-// Attach Put_Item_Policy to basic_lambda_role
+// Attach Put_Item_Policy to create_application_lambda_role
 resource "aws_iam_role_policy_attachment" "DynamoDBPutItemPolicyAttachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+  role     = aws_iam_role.create_application_lambda_role.name
   policy_arn = aws_iam_policy.Put_Item_Policy.arn
 }
 
-// Attach List_Items_Policy to basic_lambda_role
-resource "aws_iam_role_policy_attachment" "DynamoDBListItemsPolicyAttachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+// listApplications role attachments
+
+// Attach basic Lambda execution policy to list_applications_lambda_role
+resource "aws_iam_role_policy_attachment" "listApplications_basic_lambda_policy_attachment" {
+  role     = aws_iam_role.list_applications_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Attach List_Item_Policy to list_applications_lambda_role
+resource "aws_iam_role_policy_attachment" "DynamoDBListItemPolicyAttachment" {
+  role     = aws_iam_role.list_applications_lambda_role.name
   policy_arn = aws_iam_policy.List_Items_Policy.arn
 }
 
-// Attach Get_Item_Policy to basic_lambda_role
+// getApplication role attachments
+
+// Attach basic Lambda execution policy to get_application_lambda_role
+resource "aws_iam_role_policy_attachment" "getApplication_basic_lambda_policy_attachment" {
+  role     = aws_iam_role.get_application_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Attach Get_Item_Policy to get_application_lambda_role
 resource "aws_iam_role_policy_attachment" "DynamoDBGetItemPolicyAttachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+  role     = aws_iam_role.get_application_lambda_role.name
   policy_arn = aws_iam_policy.Get_Item_Policy.arn
 }
 
-// Attach Update_Item_Policy to basic_lambda_role
+// updateApplication role attachments
+
+// Attach basic Lambda execution policy to get_application_lambda_role
+resource "aws_iam_role_policy_attachment" "updateApplication_basic_lambda_policy_attachment" {
+  role     = aws_iam_role.update_application_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Attach Update_Item_policy to update_application_lambda_role
 resource "aws_iam_role_policy_attachment" "DynamoDBUpdateItemPolicyAttachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+  role     = aws_iam_role.update_application_lambda_role.name
   policy_arn = aws_iam_policy.Update_Item_Policy.arn
 }
 
-//. Attach Delete_Item_Policy to basic_lambda_role
+// deleteApplication role attachments
+
+// Attach basic Lambda execution policy to delete_application_lambda_role
+resource "aws_iam_role_policy_attachment" "deleteApplication_basic_lambda_policy_attachment" {
+  role     = aws_iam_role.delete_application_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+// Attach Delete_Item_Policy to delete_application_lambda_role
 resource "aws_iam_role_policy_attachment" "DynamoDBDeleteItemPolicyAttachment" {
-  role     = aws_iam_role.basic_lambda_role.name
+  role     = aws_iam_role.delete_application_lambda_role.name
   policy_arn = aws_iam_policy.Delete_Item_Policy.arn
 }
 
@@ -188,7 +298,7 @@ resource "aws_dynamodb_table" "job_applications_table" {
 // Lambda function resource createApplication
 resource "aws_lambda_function" "create_application" {
   function_name = "createApplication"
-  role          = aws_iam_role.basic_lambda_role.arn
+  role          = aws_iam_role.create_application_lambda_role.arn
   handler       = "dist/handlers/createApplication.handler"
   runtime       = "nodejs22.x"
   filename      = "function.zip"
@@ -203,7 +313,7 @@ resource "aws_lambda_function" "create_application" {
 // Lambda function resource listApplications
 resource "aws_lambda_function" "list_applications" {
   function_name = "listApplications"
-  role          = aws_iam_role.basic_lambda_role.arn
+  role          = aws_iam_role.list_applications_lambda_role.arn
   handler       = "dist/handlers/listApplications.handler"
   runtime       = "nodejs22.x"
   filename      = "function.zip"
@@ -218,7 +328,7 @@ resource "aws_lambda_function" "list_applications" {
 // Lambda function resource getApplication
 resource "aws_lambda_function" "get_application" {
   function_name = "getApplication"
-  role          = aws_iam_role.basic_lambda_role.arn
+  role          = aws_iam_role.get_application_lambda_role.arn
   handler       = "dist/handlers/getApplication.handler"
   runtime       = "nodejs22.x"
   filename      = "function.zip"
@@ -233,7 +343,7 @@ resource "aws_lambda_function" "get_application" {
 // Lambda function resource updateApplication
 resource "aws_lambda_function" "update_application" {
   function_name = "updateApplication"
-  role          = aws_iam_role.basic_lambda_role.arn
+  role          = aws_iam_role.update_application_lambda_role.arn
   handler       = "dist/handlers/updateApplication.handler"
   runtime       = "nodejs22.x"
   filename      = "function.zip"
@@ -248,7 +358,7 @@ resource "aws_lambda_function" "update_application" {
 // Lambda function resource deleteApplication
 resource "aws_lambda_function" "delete_application" {
   function_name = "deleteApplication"
-  role          = aws_iam_role.basic_lambda_role.arn
+  role          = aws_iam_role.delete_application_lambda_role.arn
   handler       = "dist/handlers/deleteApplication.handler"
   runtime       = "nodejs22.x"
   filename      = "function.zip"
